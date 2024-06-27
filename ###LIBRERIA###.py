d49b543d950f0e31c039baa0aec60ac72a714963
kote121984 <mar.arenasp@duocuc.cl>
2024-06-27 19:46:58 -0400

    primer commit

---
+++ ###LIBRERIA###.py
@@ -0,0 +1,19 @@
+###LIBRERIA###
+
+import json
+from datetime import datetime     
+
+
+### LISTA###
+ventas = [] 
+
+
+##### DICCIONARIO###
+precios_pizzas= {     
+    "peperoni":{"pequeña":5000, "mediana":8000,}
+    "mediterranea": {"pequeña": 6000, "mediana":90000,}
+    "vegetariana": {"pequeña":5500, "mediana":8500,}
+    }
+
+
+
